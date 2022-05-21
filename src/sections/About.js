fcab3bcdeb3fd91cbc93ac6b0dc3f91b14fabc26
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2022-05-21 06:25:39 -0700

    remove link in about section

--- src/sections/About.js
+++ src/sections/About.js
@@ -9,7 +9,7 @@ export default function About() {
     <section id="about" className="about">
         <h3>About Me <span>↓</span></h3>
         <img src={me} alt="Kelby Hawn" />
-        <p>I'm a front-end developer + designer with a <ATag href="https://www.crunchbase.com/person/kelby-hawn" linkText="background in startups" />. I love translating design into code and solving problems in a practical and mindful way while collaborating with others. I'm also a <ATag href="https://seattleinsight.org/" linkText="member and volunteer at the Seattle Insight Meditation Society" />.</p>
+        <p>I'm a front-end developer + designer with a <ATag href="https://www.crunchbase.com/person/kelby-hawn" linkText="background in startups" />. I love translating design into code and solving problems in a practical and mindful way while collaborating with others. I'm also a member and volunteer at the Seattle Insight Meditation Society.</p>
     </section>
   )
 }
