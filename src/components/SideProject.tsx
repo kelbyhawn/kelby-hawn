c9058cf90fab6e448a021a520d6f47a8cb94965a
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2026-08-04 08:26:09 -0700

    convert files to TypeScrip and add types where needed

--- src/components/SideProject.tsx
+++ src/components/SideProject.tsx
@@ -1,12 +1,19 @@
 // Components
 import Link from "next/link";
 
+type SideProjectProps = {
+  description: string;
+  href: string;
+  linkText: string;
+  className?: string;
+};
+
 export default function SideProject({
   description,
   href,
   linkText,
   className,
-}) {
+}: SideProjectProps) {
   return (
     <div>
       <Link href={href} className={className} target="_blank">
